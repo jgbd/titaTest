248b43e8deca810df8fa9ae31482b6405dd537a8
jgbd <juanbasdel@gmail.com>
2022-09-02 16:14:49 -0500

    cambios load data y bank respository

--- app_deudas/src/main/java/com/juanbastidas/appdeudas/dataSeed/DataLoader.java
+++ app_deudas/src/main/java/com/juanbastidas/appdeudas/dataSeed/DataLoader.java
@@ -4,30 +4,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.juanbastidas.appdeudas.entity.Bank;
 import com.juanbastidas.appdeudas.entity.User;
+import com.juanbastidas.appdeudas.repository.BankRepository;
 import com.juanbastidas.appdeudas.repository.UserRepository;
 
 @Component
-public class UserDataLoader implements CommandLineRunner {
+public class DataLoader implements CommandLineRunner {
 
 	@Autowired
 	UserRepository userRespository;
+	@Autowired
+	BankRepository bankRespository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		loadUserData();
+		loadBankData();
 	}
 
 	private void loadUserData() {
 		if(userRespository.count() == 0) {
 			User user1 = new User("Juan", true);
 			User user2 = new User("Gabriel", true);
-			
 			userRespository.save(user1);
 			userRespository.save(user2);
 		}
 	}
 	
-	
+	private void loadBankData() {
+		if(bankRespository.count() == 0) {
+			Bank bank1 = new Bank("Banco 1");
+			Bank bank2 = new Bank("Banco 2");
+			bankRespository.save(bank1);
+			bankRespository.save(bank2);
+		}
+	}
 	
 }
---
+++ app_deudas/src/main/java/com/juanbastidas/appdeudas/service/BankServiceImpl.java
@@ -0,0 +1,48 @@
+package com.juanbastidas.appdeudas.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.juanbastidas.appdeudas.entity.Bank;
+import com.juanbastidas.appdeudas.repository.BankRepository;
+
+@Service
+public class BankServiceImpl implements BankService {
+	@Autowired
+	private BankRepository bankRepository;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Bank> findAll() {
+		return bankRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Bank> findAll(Pageable pageable) {
+		return bankRepository.findAll(pageable);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Bank> findById(Long id) {
+		return bankRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public Bank save(Bank bank) {
+		return bankRepository.save(bank);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		bankRepository.deleteById(id);
+	}
+}
